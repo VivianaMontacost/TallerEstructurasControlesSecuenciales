e711f6701793dae7c72afb32d0f19f45dd89c1d5
VIVIANA ANDREA ANDREA <>
2021-05-03 04:33:59 +0000

    Ejercicio 16

---
+++ Ejercicio_16.py
@@ -0,0 +1,5 @@
+cantidadgalones= float(input("Ingrese el número de galones comprados:"))
+preciolitro=50000
+preciogalon=(50000*3.785)
+totalpagar=(cantidadgalones*preciogalon)
+print("El valor a pagar es de:", str(totalpagar), "pesos.")
